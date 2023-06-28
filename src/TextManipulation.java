cb7a4d99c27ea894e041a82934c1c567f412b7b6
TurcanuCatalin <turcanucatalin11@gmail.com>
2023-06-28 15:01:36 +0300

    firstCommit

---
+++ src/TextManipulation.java
@@ -0,0 +1,16 @@
+public class TextManipulation {
+    public static void main(String[] args) {
+        //afisam la ecran  mesajul situat in ""
+        System.out.println("Hello'World!");
+        System.out.println("I am learning Java");
+        System.out.println("Java is fun!");
+        // atribim variabilei name o valoare (de tip string)
+        String name="Catalin";
+        System.out.println("Hello, I am "+name);
+        // atribuim variabilei age o valoare (de tip integer)
+        int age=18;
+        // oferim o cantitate mai mica de memorie variabilei age
+        byte varsta=(byte)(age);
+        System.out.println(name+" are "+age+" ani");
+    }
+}
